c42ea34fd163153a0402c31e2d7a55c16561f2d1
USDTQBOT <150932834+USDTQBOT@users.noreply.github.com>
2025-06-02 11:07:55 +0100

    Delete app/action.ts

--- app/action.ts
+++
@@ -1,7 +0,0 @@
-'use server'
-import { db } from '@/lib/db' // Your database client
- 
-export async function createUser(data: { name: string; email: string }) {
-  const user = await db.user.create({ data })
-  return user
-}
